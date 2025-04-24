8dc864cc5e342a5ffe6222a576d9cc0376a5cd3e
nrhdyt3012 <dayats354313@gmail.com>
2025-04-24 14:28:50 +0700

    setup project frontend completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import {Button} from "@heroui/react";
-
+import PageHead from "@/components/commons/PageHead";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -9,7 +9,8 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-    <Button color="primary">Button</Button>;
+<PageHead title="BOLLEP"/> 
+  <Button color="primary">Button</Button>;
   </main>
   );
 }
